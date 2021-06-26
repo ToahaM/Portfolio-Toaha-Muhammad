1bbee511feccfd5818136f6f3a744a16a1dcbd33
ToahaM <86479594+ToahaM@users.noreply.github.com>
2021-06-25 21:49:14 -0400

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,72 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "sports",
+				weight: 12.3
+			}, {
+				text: "singing",
+				weight: 8
+			}, {
+				text: "python",
+				weight: 14
+			}, {
+				text: "coding",
+				weight: 7
+			}, {
+				text: "civic engagement",
+				weight: 10
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"love to spend time with my family.", "love to find new opportunites to help out my community.", "am optimistic.", "am determined."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#00FDDA"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
